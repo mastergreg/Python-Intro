15ac225f05899c85f9502a4d63c316deb8c4c729
Gregory Liras <gregliras@gmail.com>
2011-12-09 11:20:02 +0200

    init

---
+++ Scripts/helloworld.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+#
+#* File Name : helloworld.py
+#
+#* Purpose :
+#
+#* Creation Date : 09-12-2011
+#
+#* Last Modified : Fri 09 Dec 2011 11:17:50 AM EET
+#
+#* Created By : Greg Liras <gregliras@gmail.com>
+#
+#_._._._._._._._._._._._._._._._._._._._._.*/
+
+def main():
+    name = raw_input("Tell me your name: ")
+    print "Hello",name
+
+
+if __name__=="__main__":
+  main()
+
