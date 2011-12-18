2526ff23b4a747449d284fe7c05857044268a75e
Gregory Liras <gregliras@gmail.com>
2011-12-18 03:24:12 +0200

    alignment in helloworld

--- Scripts/helloworld.py
+++ Scripts/helloworld.py
@@ -7,15 +7,15 @@
 #
 #* Creation Date : 09-12-2011
 #
-#* Last Modified : Fri 09 Dec 2011 11:17:50 AM EET
+#* Last Modified : Sun 18 Dec 2011 03:23:59 AM EET
 #
 #* Created By : Greg Liras <gregliras@gmail.com>
 #
 #_._._._._._._._._._._._._._._._._._._._._.*/
 
 def main():
-    name = raw_input("Tell me your name: ")
-    print "Hello",name
+  name = raw_input("Tell me your name: ")
+  print "Hello",name
 
 
 if __name__=="__main__":
